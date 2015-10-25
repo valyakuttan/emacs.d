d63a394ef0268d608568afa230e3715201725be7
valyakuttan <kuttiparichathilbimal@gmail.com>
2015-10-25 11:13:00 +0530

    define a function to perform operation on each entry

--- init.d/set-path.el
+++ init.d/set-path.el
@@ -1,5 +1,17 @@
 ;; set-path.el
 
+(defun add-to-path-and-exec-path (&rest path-entries)
+  "add path-entries to both PATH and exec-path, if not present."
+  (let* ((new-path (split-string (getenv "PATH") ":"))
+         (add-entry (lambda (e)
+                      (progn
+                        (add-to-list 'new-path e)
+                        (add-to-list 'exec-path e)))))
+    (if path-entries
+        (progn
+          (mapc add-entry path-entries)
+          (setenv "PATH" (mapconcat 'identity new-path ":"))))))
+
 ;;
 ;; set $PATH and exec-path
 ;;
@@ -8,13 +20,7 @@
 (setq cabal-bin (concat home "/.cabal/bin"))
 (setq node-bin (concat home "/node_modules/bin"))
 
-(setq new-path (split-string (getenv "PATH") ":"))
-(add-to-list 'new-path ghc-bin)
-(add-to-list 'new-path cabal-bin)
-(add-to-list 'new-path node-bin)
-
-(setenv "PATH" (mapconcat 'identity new-path ":"))
-
-(add-to-list 'exec-path ghc-bin)
-(add-to-list 'exec-path cabal-bin)
-(add-to-list 'exec-path node-bin)
+(add-to-path-and-exec-path
+ ghc-bin
+ cabal-bin
+ node-bin)
